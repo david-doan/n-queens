10966eab8a73264e68ddb237acd4c8fc3ca854e9
david-doan <a.david.doan@gmail.com>
2016-04-28 12:47:26 -0700

    some n-queens optimization

--- src/createManyArray.js
+++ src/createManyArray.js
@@ -1,51 +1,86 @@
 var createManyArray = function(n) {
   var answer = [];
-  var uniqRows = [];
+  var uniqRows = {};
   var n = n;
   var emptyNxN = new Board({n: n});
+  var fullValidArr = makeEmptyValid(n);
 
-  var createBoards = function(partialBoard, currDec) {
+  var createBoards = function(partialBoard, currDec, objValid) {
     var partialBrd = partialBoard;
     var partialArr = partialBrd.rows();
     var currD = currDec;
 
     var emptySpaces = Math.pow(n, 2) - currD;
 
-    for (var spaces = 0; spaces < emptySpaces; spaces++ ) {
-      var newArr = fillNextEmptySpace(partialArr, spaces, n);
+    for (var validSpace in objValid ) {
+      var newArrValidContainer = fillNextEmptySpace(partialArr, validSpace, n, objValid);
+      var newArr = newArrValidContainer[0];
+      var newValidObj = newArrValidContainer[1];
+
       var newBrd = new Board(newArr);
       if ( newBrd.hasAnyConflicts() ) {
         continue;  
       }
-      // for some reason the JSON.stringify memo of newArr is not working
-      if (currD >= n - 1 && uniqRows.indexOf( JSON.stringify(newArr)) === -1) {
+
+      // check if we put our last piece down and answer is NOT duplicate
+      if (currD >= n-1 && uniqRows[ JSON.stringify(newArr) ] === undefined) {
+        // debugger;
+        console.log('I am on ' + currD + ' piece.');
         answer.push(newBrd);
-        uniqRows.push( JSON.stringify(newArr) );
-      } else {
-        createBoards (newBrd, currD + 1);    
+        uniqRows[ JSON.stringify(newArr) ] = 1;
+      } else if (currD < n - 1) {
+        createBoards (newBrd, currD + 1, newValidObj);    
       }
     }        
   };
 
-  createBoards(emptyNxN, 0, 0);
+  createBoards(emptyNxN, 0, fullValidArr);
   return answer;
 };
 
-
-var fillNextEmptySpace = function (partialArray, numPossFilled, n) {
+// objValid  is obj contains array of valid possible indices { 0:[r,c], 1:[r,c], 2:[r,c], 3:[r,c] }
+var fillNextEmptySpace = function (partialArray, nextValidSpace, n, objValid) {
   var answer = makeCopy(partialArray);
-  var emptySpaceCounter = 0;
-  for (var r = 0; r < n; r++) {
-    for (var c = 0; c < n; c++) {
-      if (answer[r][c] === 0) {
-        emptySpaceCounter++; // count number of empty spaces
-      }
-      if (emptySpaceCounter === numPossFilled + 1) {
-        answer[r][c] = 1; // add piece
-      }
+  // var rowPrevPiece = indexPrevPiece[0];
+  // var colPrevPiece = indexPrevPiece[1];
+  var nextValidObj = JSON.parse(JSON.stringify(objValid));
+
+  // debugger;
+
+  // this loop will put down the current piece
+  var newR = objValid[nextValidSpace][0];
+  var newC = objValid[nextValidSpace][1];
+  answer[newR][newC] = 1; // add piece to valid position
+
+  for ( var index in nextValidObj) {
+    if (nextValidObj[index][0] === newR || nextValidObj[index][1] === newC) {
+      delete nextValidObj[index];
     }
   }
-  return answer;
+
+
+  // if we are not at last piece but there is no valid places to put that piece, 
+  // we need to fail that tree
+
+  // for (var r = 0; r < n; r++) {
+  //   if(r === rowPrevPiece){
+  //     r++;
+  //     continue;
+  //   }
+  //   for (var c = 0; c < n; c++) {
+  //     if (c === colPrevPiece) {
+  //       c++;
+  //       continue;
+  //     }
+  //     if (answer[r][c] === 0) {
+  //       emptySpaceCounter++; // count number of empty spaces
+  //     }
+  //     if (emptySpaceCounter === numPossFilled + 1) {
+  //       answer[r][c] = 1; // add piece
+  //     }
+  //   }
+  // }
+  return [answer, nextValidObj];
 };
 
 var makeCopy = function(array) {
@@ -71,4 +106,17 @@ var makeEmptyMatrix = function(n) {
       return 0;
     });
   });
+};
+
+var makeEmptyValid = function(n) {
+  var answer = {};
+  var index = 0;
+  for (var r = 0; r < n; r++) {
+    for (var c = 0; c < n; c++) {
+      answer[index] = [r,c];
+      index++;
+    }
+  }
+  //answer.length = index;
+  return answer;
 };
