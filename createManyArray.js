c930c3594d2945d55949a0b0abf2eece16580142
david-doan <a.david.doan@gmail.com>
2016-04-27 19:16:57 -0700

    completes createManyArray function that creates all board possiblities for n

--- createManyArray.js
+++ createManyArray.js
@@ -1,80 +1,67 @@
-var createManyArray = function(n){
-    var answer = [];
-    var n = n;
-    var emptyNxN = makeEmptyMatrix(n);
+var createManyArray = function(n) {
+  var answer = [];
+  var n = n;
+  var emptyNxN = makeEmptyMatrix(n);
 
-    var createArray = function(partialArray, numPossFilled, currDec){
-        var partialArr = partialArray;
-        var currD = currDec;
-//         var arr = [];
-        var emptySpaces = n^2-currD;
-        var possCounter = 0; //
-        //base cases
-        if(currD >= n - 1){
-            // push completed array into answer
-        }
-        //base case
-        if(spaceCounter >= emptySpaces){
-            //we are done with this decision branch, go back up to currD-1
-                // (gotta go back one decision and put piece in new spot then recurse down)
-                createArray()
-        }
-        
-        if(possCounter < emptySpaces){
-            var newArr = fillNextEmptySpace(partialArr, possCounter, n);
-            possCounter++;
-            createArray(newArr,0, currD);    
-        }
-        
-        
+  var createArray = function(partialArray, currDec) {
+    var partialArr = partialArray;
+    var currD = currDec;
 
-        createArray(partialArr, 0, currDec + 1);
+    var emptySpaces = Math.pow(n, 2) - currD;
 
-
-
-    }
-
-    createArray(emptyNxN, 0, 0);
-}
-
-
-var fillNextEmptySpace = function (partialArray, numPossFilled, n){
-    var answer = makeCopy(partialArray);
-    var emptySpaceCounter = 0;
-    for(var r = 0; r < n; r++){
-        for(var c = 0; c < n; c++){
-            if(answer[r][c] === 0){
-                emptySpaceCounter++; // count number of empty spaces
-            }
-            if(emptySpaceCounter === numPossFilled +1 ){
-                answer[r][c] = 1; // add piece
-            }
-        }
-    }
-    return answer;
-};
-
-var makeCopy = function(array){
-    var answer = array.slice();
-    for(var i =0; i < array.length; i++){
-        if(Array.isArray(array[i])){
-            answer[i] = array[i].slice();
-        } else {
-            answer[i] = array[i];
-        }
-    }
-
-    return answer;
-};
-
-var toggle = function(value){
-    return value === 0 ? 1 : 0;
-}
-
-  var makeEmptyMatrix = function(n) {
-    return _(_.range(n)).map(function() {
-      return _(_.range(n)).map(function() {
-        return 0;
-      });
-    });
+    for (var spaces = 0; spaces < emptySpaces; spaces++ ) {
+      var newArr = fillNextEmptySpace(partialArr, spaces, n);
+      if (currD >= n - 1) {
+        answer.push(newArr);
+      } else {
+        createArray (newArr, currD + 1);    
+      }
+    }        
   };
+
+  createArray(emptyNxN, 0, 0);
+
+  return answer;
+};
+
+
+var fillNextEmptySpace = function (partialArray, numPossFilled, n) {
+  var answer = makeCopy(partialArray);
+  var emptySpaceCounter = 0;
+  for (var r = 0; r < n; r++) {
+    for (var c = 0; c < n; c++) {
+      if (answer[r][c] === 0) {
+        emptySpaceCounter++; // count number of empty spaces
+      }
+      if (emptySpaceCounter === numPossFilled + 1) {
+        answer[r][c] = 1; // add piece
+      }
+    }
+  }
+  return answer;
+};
+
+var makeCopy = function(array) {
+  var answer = array.slice();
+  for (var i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      answer[i] = array[i].slice();
+    } else {
+      answer[i] = array[i];
+    }
+  }
+
+  return answer;
+};
+
+var toggle = function(value) {
+  return value === 0 ? 1 : 0;
+};
+
+var makeEmptyMatrix = function(n) {
+  return _(_.range(n)).map(function() {
+    return _(_.range(n)).map(function() {
+      return 0;
+    });
+  });
+};
